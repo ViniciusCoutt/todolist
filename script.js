117e2f4c159439cfab506f1bac18d3aef2c7973e
Vinicius Coutinho <vini.coutinho.if@gmail.com>
2021-10-05 21:39:33 -0300

    Add - melhorias no estilo; agora a tarefa é salva na sessão local, informa o a data e o dia, informa a quantidade de tarefas restantes.

--- script.js
+++ script.js
@@ -1,35 +1,90 @@
-let sendTask = document.querySelector("#sendTask")
-let i = 1;
+const addBtn = document.querySelector("#addTask")
+const listBox = document.querySelector("#taskList")
+const taskInput = document.querySelector("#txtInput")
+const dateBox = document.querySelector(".date");
 
-sendTask.addEventListener("click", send)
+showList()
 
-function send() {
-    let task = document.querySelector("#txtInput")
+addBtn.addEventListener('click', getInput)
+taskInput.onkeypress = function (e) {
+    e.key == 'Enter' ? getInput() : null
+}
 
-    if(task.value == "") {
-        alert("Campo vazio. Informe a tarefa")
+function getInput() {
+    taskInput.value == "" ? alert("Campo vazio. Informe a tarefa") : userTask = taskInput.value
+
+    const localStorageData = localStorage.getItem("New task")
+    localStorageData == null ? taskArr = [] : taskArr = JSON.parse(localStorageData)
+    taskArr.push(userTask)
+    localStorage.setItem("New task", JSON.stringify(taskArr))
+
+    showList()
+}
+
+function showList() {
+    const localStorageData = localStorage.getItem("New task")
+    localStorageData == null ? taskArr = [] : taskArr = JSON.parse(localStorageData)
+
+    const remainingTasks = document.querySelector(".remainingTasks")
+    remainingTasks.textContent = "tarefas restantes: " + taskArr.length
+
+    let newLi = "";
+    taskArr.forEach((task, index) => {
+        newLi +=
+            `<li> 
+                <label for=checkbox${index}> <input type='checkbox' id=checkbox${index}> ${task} </label> 
+                <span onclick="removeTask(${index})">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>`
+    })
+    listBox.innerHTML = newLi
+    taskInput.value = ""
+}
+
+function removeTask(index) {
+    const localStorageData = localStorage.getItem("New task")
+    taskArr = JSON.parse(localStorageData)
+    taskArr.splice(index, 1)
+    localStorage.setItem("New task", JSON.stringify(taskArr))
+
+    showList()
+}
+
+// Input effect
+taskInput.onkeyup = () => {
+    console.log(taskInput.value.trim())
+    if (taskInput.value.trim() != 0) {
+        addBtn.classList.add('active')
     } else {
-        createList(task.value)
+        addBtn.classList.remove('active')
     }
 }
 
-function createList(task) {
-    let taskList = " "
-    taskList += "<li>"
-    taskList += `<label for=checkbox${i}> <input type='checkbox' id=checkbox${i}> ` + task + `</label>`
-    taskList += ` <span onclick="remove(event)"><i class="fas fa-trash-alt"></i></span>`
-    taskList += "</li>"
-
-    document.getElementById('taskList').innerHTML += taskList
-    i++
-}
-
-remove = function (event) {
-    toRemove = event.currentTarget.parentElement
-
-    toRemove.remove()
-
-}
-
-
-
+(function date() {
+    let day;
+    let dayN = new Date().getDay()
+    switch (dayN) {
+        case 0:
+            day = ('Domingo')
+            break
+        case 1:
+            day = ('Segunda')
+            break
+        case 2:
+            day = ('Terça')
+            break
+        case 3:
+            day = ('Quarta')
+            break
+        case 4:
+            day = ('Quinta')
+            break
+        case 5:
+            day = ('Sexta')
+            break
+        case 6:
+            day = ('Sábado')
+    }
+    dateBox.innerHTML = `${day}, 0` + new Date().getDate()
+})()
